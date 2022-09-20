2ee2a5a3407591f2fb49de3da9cc63b02bf1c8e9
Rodrigo Branas <rodrigo.branas@gmail.com>
2022-09-20 14:57:38 -0300

    updated

---
+++ test/validate.test.ts
@@ -0,0 +1,32 @@
+import { validate } from "../src/v2/validate";
+
+test("Deve validar o cpf válido que tem dígito maior de zero", function () {
+	const isValid = validate("259.556.978-37");
+	expect(isValid).toBeTruthy();
+});
+
+test("Deve validar o cpf com dígito zero no primeiro dígito", function () {
+	const isValid = validate("198.454.187-08");
+	expect(isValid).toBeTruthy();
+});
+
+
+test("Deve validar o cpf com dígito zero no segundo dígito", function () {
+	const isValid = validate("147.085.437-60");
+	expect(isValid).toBeTruthy();
+});
+
+test("Deve tentar validar o cpf com mais de 14 caracteres", function () {
+	const isValid = validate("147.085.437-600");
+	expect(isValid).toBeFalsy();
+});
+
+test("Deve validar o cpf com dígito zero no segundo dígito", function () {
+	const isValid = validate("111.111.111-11");
+	expect(isValid).toBeFalsy();
+});
+
+test("Deve validar o cpf com letras", function () {
+	const isValid = validate("abc");
+	expect(isValid).toBeFalsy();
+});
